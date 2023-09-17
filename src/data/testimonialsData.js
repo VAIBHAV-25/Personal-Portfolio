5e6ed51b532d5c74e91dc72d51bae4596f2567bf
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-09-17 13:13:17 +0530

    testinomials changes

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -11,41 +11,48 @@ import boy3 from '../assets/svg/testimonials/boy3.svg'
 export const testimonialsData = [
     {
         id: 1,
+        name: 'Prashant R. Mohta',
+        title: 'Testbook',
+        text: "Vaibhav joined us as an associate a frontend developer intern. He is passionate, creative and always keen on learning new things.",
+        image: boy3
+    },
+    {
+        id: 2,
         name: 'Bharat Jain',
         title: 'Milaan Technologies',
         text: "Vaibhav has worked more than 4 months in Milaan Technologies, he is an excellent learner and harworking person",
         image: boy2
     },
     {
-        id: 2,
+        id: 3,
         name: 'Chandni Jain',
         title: 'Milaan Technologies',
         text: "Vaibhav was the part of Data Mining team and had worked in field of ML and NLP. His performance was exception during the period.",
         image: girl2
     },
     {
-        id: 3,
+        id: 4,
         name: 'Kahmira Mewara',
         title: 'LakeBrain Technolgies',
         text: "Vaibhav is a brilliant scholar and a dedicated person ! He is hardworking, motivated and has deep knowledge of the field.",
         image: girl1
     },
     {
-        id: 4,
+        id: 5,
         name: 'Abhinav Sharma',
         title: 'We Code',
         text: "Vaibhav had worked in We Code as an intern in field of web development. He was honest and had good knowledge of the subject.",
         image: boy1
     },
     {
-        id: 5,
+        id: 6,
         name: 'Harsh Shrivastava',
         title: 'Coditas',
         text: "Vaibhav has joined us as an associate software engineering intern. He is hardworking, focused and dedicated person",
         image: boy3
     },
     {
-        id: 6,
+        id: 7,
         name: 'Vandana Singh',
         title: 'Tech Spark',
         text: "During his intern, Vaibhav had shown atmost honesty towards his duties. His work is really appreciable.",
