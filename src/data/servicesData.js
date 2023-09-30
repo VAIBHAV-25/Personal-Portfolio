e03e21a6eb8de8da79d72ff6eaf7519788920949
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-09-30 11:26:20 +0530

    fix: dependancy fixes

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -75,7 +75,3 @@ export const servicesData = [
     },
 
 ]
-
-// Uncomment your required service.
-// Couldn't find the required services? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-// You can also add on your own 😉.
