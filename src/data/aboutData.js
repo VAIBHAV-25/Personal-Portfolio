4fdaadc78d945c280a7295f9c1d822d56f350858
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2024-01-14 10:02:56 +0530

    fix: minor fixes

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Vaibhav. I'm a web designer and developer based in Rajathan, India.",
+    description1: "My name's Vaibhav. I'm a web designer and developer based in Rajasthan, India.",
     description2: "I am deeply enthusiastic about crafting and advancing clean, distinctive, and elegant products. My specialization lies in constructing progressive Web Applications. I am dedicated to maintaining clean, readable, and modular code, ensuring it remains well-refactored. My passion lies in tackling intricate user interfaces, solving complex problems, and continuously optimizing website performance to achieve an excellent user experience.",
     image: 2
 }
